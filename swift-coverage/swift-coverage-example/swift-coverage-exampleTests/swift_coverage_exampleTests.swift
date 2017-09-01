b5bf029d220da9d37b8cb03aec3ab558f8ca33e5
Balazs Banyai <balazs.banyai@logmein.com>
2017-09-01 09:56:10 +0200

    fix #6 by migrating to latest swift syntax

--- swift-coverage/swift-coverage-example/swift-coverage-exampleTests/swift_coverage_exampleTests.swift
+++ swift-coverage/swift-coverage-example/swift-coverage-exampleTests/swift_coverage_exampleTests.swift
@@ -28,7 +28,7 @@ class swift_coverage_exampleTests: XCTestCase {
     
     func testPerformanceExample() {
         // This is an example of a performance test case.
-        self.measureBlock {
+        self.measure {
             // Put the code you want to measure the time of here.
         }
     }

2e2843a43e6ba956040acf79115d81deda1ae7db
akrjohn <30516372+akrjohn@users.noreply.github.com>
2017-09-06 14:39:04 -0700

    Changes to test commit
    Changes to test commit

--- sonarqube-scanner-maven/app-java/src/main/java/example/HelloWorld.java
+++ sonarqube-scanner-maven/app-java/src/main/java/example/HelloWorld.java
@@ -5,6 +5,7 @@ public class HelloWorld {
   public void coveredByUnitTest() {
     System.out.println("coveredByUnitTest1");
     System.out.println("coveredByUnitTest2");
+    System.out.println("coveredByUnitTest3");
   }
 
   public void coveredByIntegrationTest() {

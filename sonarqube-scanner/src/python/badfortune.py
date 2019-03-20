6d930f3e47b6aca4f22dc4704a63c6644d3d97b4
moseskim <moseskim@AL01201752.local>
2019-03-21 07:47:36 +0900

    teset commit

--- sonarqube-scanner/src/python/badfortune.py
+++ sonarqube-scanner/src/python/badfortune.py
@@ -50,6 +50,7 @@ def get(filename):
 
     datfile = open(filename+'.dat', 'r')
     data = datfile.read(5 * LONG_SIZE)
+    data2 = datfile.read(5 * LONG_SIZE)
     if is_64_bit:
         v1, v2, n1, n2, l1, l2, s1, s2, f1, f2 = struct.unpack('!10L', data)
         version  = v1 + (v2 << 32)

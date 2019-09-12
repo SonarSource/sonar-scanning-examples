b6dbf1dca18d49481691590a6e9c43ad2fc5ce8a
Emanuel Schmoczer <emanuel@schmoczer.com>
2019-09-12 16:06:26 +0200

    make xccov-to-sonarqube-generic.sh work with Xcode 11 (#63)

--- swift-coverage/swift-coverage-example/xccov-to-sonarqube-generic.sh
+++ swift-coverage/swift-coverage-example/xccov-to-sonarqube-generic.sh
@@ -5,7 +5,12 @@ function convert_file {
   local xccovarchive_file="$1"
   local file_name="$2"
   echo "  <file path=\"$file_name\">"
-  xcrun xccov view --file "$file_name" "$xccovarchive_file" | \
+  local line_coverage_cmd="xcrun xccov view"
+  if [[ $@ == *".xcresult"* ]]; then
+    line_coverage_cmd="$line_coverage_cmd --archive"
+  fi
+  line_coverage_cmd="$line_coverage_cmd --file \"$file_name\" \"$xccovarchive_file\""
+  eval $line_coverage_cmd | \
     sed -n '
     s/^ *\([0-9][0-9]*\): 0.*$/    <lineToCover lineNumber="\1" covered="false"\/>/p;
     s/^ *\([0-9][0-9]*\): [1-9].*$/    <lineToCover lineNumber="\1" covered="true"\/>/p
@@ -16,7 +21,12 @@ function convert_file {
 function xccov_to_generic {
   echo '<coverage version="1">'
   for xccovarchive_file in "$@"; do
-    xcrun xccov view --file-list "$xccovarchive_file" | while read -r file_name; do
+    local file_list_cmd="xcrun xccov view"
+    if [[ $@ == *".xcresult"* ]]; then
+      file_list_cmd="$file_list_cmd --archive"
+    fi
+    file_list_cmd="$file_list_cmd --file-list \"$xccovarchive_file\""
+    eval $file_list_cmd | while read -r file_name; do
       convert_file "$xccovarchive_file" "$file_name"
     done
   done

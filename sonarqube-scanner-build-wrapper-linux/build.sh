9ffa5b024c717167a6df544f69215a47259729cc
Alix Lourme <alix.lourme@gmail.com>
2020-08-03 12:17:37 +0200

    Fix #87 : Build wrapper Linux - Build without link

--- sonarqube-scanner-build-wrapper-linux/build.sh
+++ sonarqube-scanner-build-wrapper-linux/build.sh
@@ -4,9 +4,9 @@ rm -rf build
 
 echo "BUILDING"
 mkdir build
-g++ -Wall -o build/BiggestUnInt src/BiggestUnInt.cc
-g++ -Wall -o build/HelloWorld src/HelloWorld.cpp
-g++ -Wall -o build/SimpleClass src/SimpleClass.cc
+g++ -c -Wall -o build/BiggestUnInt src/BiggestUnInt.cc
+g++ -c -Wall -o build/HelloWorld src/HelloWorld.cpp
+g++ -c -Wall -o build/SimpleClass src/SimpleClass.cc
 RC=$?
 
 if [ $RC -ne 0 ]; then

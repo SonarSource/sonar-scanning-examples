a74b00a6c0eaa3d80664cd614da97ec47563d505
Joe Tingsanchali <joe.tingsanchali@sonarsource.com>
2021-11-30 16:01:56 -0600

    Add example of Ant with JaCoCo coverage and Sonar scanner for Ant

---
+++ sonarqube-scanner-ant/ant-coverage/src/main/java/org/jacoco/examples/expressions/Const.java
@@ -0,0 +1,27 @@
+/*******************************************************************************
+ * Copyright (c) 2009, 2021 Mountainminds GmbH & Co. KG and Contributors
+ * This program and the accompanying materials are made available under
+ * the terms of the Eclipse Public License 2.0 which is available at
+ * http://www.eclipse.org/legal/epl-2.0
+ *
+ * SPDX-License-Identifier: EPL-2.0
+ *
+ * Contributors:
+ *    Marc R. Hoffmann - initial API and implementation
+ *    
+ *******************************************************************************/
+package org.jacoco.examples.expressions;
+
+public class Const implements IExpression {
+
+	private final double value;
+
+	public Const(final double value) {
+		this.value = value;
+	}
+
+	public double evaluate() {
+		return value;
+	}
+
+}
---
+++ sonarqube-scanner-ant/ant-coverage/src/main/java/org/jacoco/examples/expressions/Div.java
@@ -0,0 +1,30 @@
+/*******************************************************************************
+ * Copyright (c) 2009, 2021 Mountainminds GmbH & Co. KG and Contributors
+ * This program and the accompanying materials are made available under
+ * the terms of the Eclipse Public License 2.0 which is available at
+ * http://www.eclipse.org/legal/epl-2.0
+ *
+ * SPDX-License-Identifier: EPL-2.0
+ *
+ * Contributors:
+ *    Marc R. Hoffmann - initial API and implementation
+ *    
+ *******************************************************************************/
+package org.jacoco.examples.expressions;
+
+public class Div implements IExpression {
+
+	private final IExpression l;
+
+	private final IExpression r;
+
+	public Div(final IExpression l, final IExpression r) {
+		this.l = l;
+		this.r = r;
+	}
+
+	public double evaluate() {
+		return l.evaluate() / r.evaluate();
+	}
+
+}

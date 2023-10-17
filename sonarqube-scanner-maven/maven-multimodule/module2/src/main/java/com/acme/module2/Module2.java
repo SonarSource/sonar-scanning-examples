cd068a25b17c42d3fd40df711ffbaed73a504d1b
denis.troller <denis.troller@sonarsource.com>
2023-10-17 12:09:29 +0200

    Added simpleTest method

--- sonarqube-scanner-maven/maven-multimodule/module2/src/main/java/com/acme/module2/Module2.java
+++ sonarqube-scanner-maven/maven-multimodule/module2/src/main/java/com/acme/module2/Module2.java
@@ -1,6 +1,20 @@
 package com.acme.module2;
 
+import java.util.List;
+
 public class Module2 {
+
+  public List getList() {
+    return null;
+  }
+
+  public boolean simpleTest2(){
+    return getList().size() > 10;
+  }
+
+  public boolean simpleTest(List<Integer> list){
+    return (list.size() > 10);
+  }
 
   public void coveredByUnitTest() {
     System.out.println("This method is covered by unit test");

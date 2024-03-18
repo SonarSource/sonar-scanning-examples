14233627cdfb98563e73fc018a9f0a682d2fc3be
Junho Yun <116056803+Junoya7@users.noreply.github.com>
2024-03-19 02:12:44 +0900

    Update Main.java

--- sonar-scanner-gradle/gradle-multimodule-coverage/application/src/main/java/org/gradle/sample/app/Main.java
+++ sonar-scanner-gradle/gradle-multimodule-coverage/application/src/main/java/org/gradle/sample/app/Main.java
@@ -10,6 +10,6 @@ public class Main {
     public static void main(String[] args) {
         LinkedList tokens;
         tokens = split(getMessage());
-        System.out.println(join(tokens));
+        System.out.println(join(tokens)); 
     }
 }

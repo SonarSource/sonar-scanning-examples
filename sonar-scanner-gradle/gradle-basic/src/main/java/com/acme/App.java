d00624648f56b7b710bc930f01b9a13d93e55451
Junho Yun <116056803+Junoya7@users.noreply.github.com>
2024-03-19 22:12:30 +0900

    Update App.java

--- sonar-scanner-gradle/gradle-basic/src/main/java/com/acme/App.java
+++ sonar-scanner-gradle/gradle-basic/src/main/java/com/acme/App.java
@@ -2,7 +2,7 @@ package com.acme;
 
 public class App {
     public String getGreeting() {
-        return "Hello world.";
+        return "Hello world...";
     }
 
     public static void main(String[] args) {

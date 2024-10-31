9c4241501ff62e2d6a4c135f75b650b5baeaca03
Daniel Felipe Henao Toro <daniel.henao@geniussports.com>
2024-10-31 07:01:08 -0500

    Se agregan ajustes para incluir sonar que solo lean sonar-scanner-maven

--- sonar-scanner-maven/maven-basic/src/main/java/com/acme/basic/HelloWorld.java
+++ sonar-scanner-maven/maven-basic/src/main/java/com/acme/basic/HelloWorld.java
@@ -10,4 +10,8 @@ public class HelloWorld {
     System.out.println("This method is not covered by unit tests");
   }
 
+  void notCoveredChanged() {
+    System.out.println("This method is not covered by unit tests");
+  }
+
 }

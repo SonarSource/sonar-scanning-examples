794184421c0afaeaa5d37be06b00f28befae4ed9
Daniel Felipe Henao Toro <daniel.henao@geniussports.com>
2024-10-31 07:04:23 -0500

    Se agregan cambios al pipeline

--- sonar-scanner-maven/maven-basic/src/main/java/com/acme/basic/HelloWorld.java
+++ sonar-scanner-maven/maven-basic/src/main/java/com/acme/basic/HelloWorld.java
@@ -9,9 +9,4 @@ public class HelloWorld {
   void notCovered() {
     System.out.println("This method is not covered by unit tests");
   }
-
-  void notCoveredChanged() {
-    System.out.println("This method is not covered by unit tests");
-  }
-
 }
